f2b518559074c3ca1f0bf4cfb157374157a2d0d7
Tilak Raj <34798905+CodetheCloud@users.noreply.github.com>
2025-12-25 09:13:27 +0000

    initial python code commit

---
+++ core/userinput.py
@@ -0,0 +1,9 @@
+print('THis is for user input in python')
+num1 = int( input("Enter an integer ") )
+num2 = int( input("Enter another integer ") )
+
+print(f"You entered these two integer: {num1} and {num2}")
+
+sum = num1 + num2 
+
+print(f"sum of these two nums are {sum}")
