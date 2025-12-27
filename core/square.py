452d7f0d0ec42b41ffbc0bc7b2189b0770ff998c
Tilak Raj <34798905+CodetheCloud@users.noreply.github.com>
2025-12-27 11:34:16 +0000

    square pattern code added

---
+++ core/square.py
@@ -0,0 +1,16 @@
+# Square Pattern program in python
+
+matrixsize = 7
+i = 1
+
+while i <= matrixsize:  
+
+    j = 1 
+    while j <= matrixsize:
+        print('*', end=' ')
+        j = j + 1
+    
+    print(' ')
+    i = i + 1
+
+print('EOP')
