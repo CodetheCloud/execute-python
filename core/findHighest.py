ef129d3224d3df81ba70669d5087cd2bf9d64b4b
Tilak Raj <34798905+CodetheCloud@users.noreply.github.com>
2026-01-11 10:05:52 +0000

    another commit

--- core/findHighest.py
+++ core/findHighest.py
@@ -1,13 +1,10 @@
 num_array = [1,5,7,9,110,15,16,19]
 print(num_array)
 print(len(num_array))
-
-
 big = num_array[0]
 
 
 i = 0
-
 while i<len(num_array):
     print(num_array[i])
     if(num_array[i] > big):
